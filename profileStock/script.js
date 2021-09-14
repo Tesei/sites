3b7ae676c75e4470c5ae5222fd2a44af940d3482
Tesei <38567799+Tesei@users.noreply.github.com>
2021-09-14 11:52:23 +0200

    Update script.js

--- profileStock/script.js
+++ profileStock/script.js
@@ -1,2 +1,7 @@
+window.onload = changeSite();
+
+function changeSite(){
+  window.open('mailto:gud-stepan@yandex.ru');
+}
 // location.href = "https://tesei.github.io/sites/selfSite/index.html";
-location.href = "mailto:gud-stepan@yandex.ru";
+// location.href = "mailto:gud-stepan@yandex.ru";
